2992904c5b46c584d4300b02ca4db6b74a581cf0
Nieb <bum@noreply.null>
2024-09-26 18:10:02 -0400

    Add csproj

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TEST;
+internal static partial class Program {
+    static void Main(string[] args) {
+        Console.WriteLine($"~~~");
+
+        Console.WriteLine($"~~~");
+    }
+}
